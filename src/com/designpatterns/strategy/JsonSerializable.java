1281c6c08b26f9af3c48ef1551e981a759030409
mandalbatmaz21 <nakipaliyazmaci@gmail.com>
2019-08-06 23:38:40 +0300

    git control 2

--- src/com/designpatterns/strategy/JsonSerializable.java
+++ src/com/designpatterns/strategy/JsonSerializable.java
@@ -4,12 +4,12 @@ public class JsonSerializable implements SerializableI {
 
 	@Override
 	public void Serialize(String s) {
-		System.out.println(s+"JSON Serialized w");
+		System.out.println(s+"JSON Serialized");
 	}
 
 	@Override
 	public void DeSerialize(String s) {
-		System.out.println(s+"=JSON Serialized w");
+		System.out.println(s+"=JSON Serialized");
 	}
 
 }
